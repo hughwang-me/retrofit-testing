f6d727c21d8cbfcb959467e4aa2176c0f7a8855d
Hugh Wang <18501667737@163.com>
2021-05-04 22:06:45 +0800

    by bash

---
+++ src/main/java/com/uwjx/retrofittesting/RetrofitTestingApplication.java
@@ -0,0 +1,13 @@
+package com.uwjx.retrofittesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetrofitTestingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RetrofitTestingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uwjx/retrofittesting/RetrofitTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uwjx.retrofittesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetrofitTestingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
